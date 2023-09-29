bbb52681ac0365e6af7e3acf3aa003f0e4fd32dd
naveenraj39 <123942201+naveenraj39@users.noreply.github.com>
2023-09-29 20:09:38 +0530

    Third

--- TmxHubrisWrecker-master/src/test/java/com/triomics/framework/common/AppConfigConstants.java
+++
@@ -1,8 +0,0 @@
-package com.triomics.framework.common;
-
-public interface AppConfigConstants {
-
-    String ENV_CONFIGSERVER_ENDPOINT = "env.configserver.endpoint";
-
-    String ENV_TYPE = "env.type";
-}
